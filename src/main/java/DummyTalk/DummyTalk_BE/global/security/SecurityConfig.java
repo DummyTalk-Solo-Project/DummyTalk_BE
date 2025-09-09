6e1502fcdd4dab827abc00f097a19138f48634be
jijysun <jijysun@naver.com>
2025-09-09 23:17:16 +0900

    [FEAT] : JWT 발급 코드 구현

--- src/main/java/DummyTalk/DummyTalk_BE/global/security/SecurityConfig.java
+++ src/main/java/DummyTalk/DummyTalk_BE/global/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +14,10 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
-
         http
+                .sessionManagement(session->{
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((auth) ->
                         auth.requestMatchers("/**", "/favicon.ico").permitAll());
@@ -21,4 +25,8 @@ public class SecurityConfig{
 
     }
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/DummyTalk/DummyTalk_BE/global/security/jwt/JWT.java
+++ src/main/java/DummyTalk/DummyTalk_BE/global/security/jwt/JWT.java
@@ -1,14 +1,49 @@
 package DummyTalk.DummyTalk_BE.global.security.jwt;
 
+import io.jsonwebtoken.Jwts;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Getter
 @Builder
 @AllArgsConstructor
 public class JWT {
-    private String grantType;
-    private String accessToken;
-    private String refreshToken;
+    private SecretKey secretKey;
+
+    public JWT(@Value("${jwt.secret.key}") String secretKey) {
+        this.secretKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm()) ;
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey)
+                .build().parseSignedClaims(token).getPayload().get("username", String.class); // or Email
+    }
+
+    public String getRole (String token){
+        return Jwts.parser().verifyWith(secretKey)
+                .build().parseSignedClaims(token).getPayload().get("role", String.class); // or Email
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey)
+                .build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJWT (String username, String role, Long expireMs){
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expireMs))
+                .signWith(secretKey)
+                .compact();
+    }
 }
