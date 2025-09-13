0544b6986c22abdf3665764473b46009e49d8b4a
jijysun <jijysun@naver.com>
2025-09-14 02:08:35 +0900

    [FEAT] : 잡상식 요청 형식 구현

--- src/main/java/DummyTalk/DummyTalk_BE/domain/controller/DummyController.java
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/controller/DummyController.java
@@ -1,10 +1,15 @@
 package DummyTalk.DummyTalk_BE.domain.controller;
 
+import DummyTalk.DummyTalk_BE.domain.dto.dummy.DummyRequestDTO;
+import DummyTalk.DummyTalk_BE.domain.entity.user.User;
+import DummyTalk.DummyTalk_BE.global.security.userDetails.CustomUserDetails;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,17 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "더미 API", description = "일반적인 잡지식을 보게 되는 단방향 대화 API 입니다")
 public class DummyController {
 
-    @GetMapping ("/normal-dummy")
-    public ResponseEntity<Object> dummyTalk (){
-        return ResponseEntity.ok(null);
+    @GetMapping ("/get-dummy")
+    public ResponseEntity<Object> dummyTalk (@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody DummyRequestDTO.RequestInfoDTO requestInfoDTO) {
+
+        User user = userDetails.getUser();
+        
+        // 일반 요청인 경우
+        
+        // 개발자, 고급 요청인 경우
+        
+        // 악의적 접근인 경우
+
+        return ResponseEntity.ok("AI 메세지 삽입 예정");
     }
-
-    @GetMapping ("/security-dummy")
-    public ResponseEntity<Object> securityDummy (){
-        return ResponseEntity.ok(null);
-    }
-
-
     
     
 }
---
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/entity/constant/TriggerType.java
@@ -0,0 +1,7 @@
+package DummyTalk.DummyTalk_BE.domain.entity.constant;
+
+public enum TriggerType {
+    PAGE_RELOAD, // F5 새로고침
+    BUTTON_CLICK, // 버튼 클릭
+    URL_ENTER // 주소에서 엔터
+}
