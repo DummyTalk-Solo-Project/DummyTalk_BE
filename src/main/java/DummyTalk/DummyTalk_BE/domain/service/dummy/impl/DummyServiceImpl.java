c84e093ba83b2acbcc01d9b98c1f6ce737e35272
jijysun <jijysun@naver.com>
2025-09-14 02:14:12 +0900

    [FEAT] : 전체적인 뼈대 구현, 집중 개발 시작!

--- src/main/java/DummyTalk/DummyTalk_BE/domain/service/dummy/impl/DummyServiceImpl.java
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/service/dummy/impl/DummyServiceImpl.java
@@ -1,18 +1,36 @@
 package DummyTalk.DummyTalk_BE.domain.service.dummy.impl;
 
+import DummyTalk.DummyTalk_BE.domain.dto.dummy.DummyRequestDTO;
+import DummyTalk.DummyTalk_BE.domain.entity.user.User;
+import DummyTalk.DummyTalk_BE.domain.repository.UserRepository;
 import DummyTalk.DummyTalk_BE.domain.service.dummy.DummyService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.stereotype.Service;
 
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class DummyServiceImpl implements DummyService {
 
+    private final UserRepository userRepository;
+    private final OpenAiApi openAi;
+
     @Override
-    public String GetDummyData() {
+    public String GetDummyDateForNormal(User user, DummyRequestDTO.RequestInfoDTO requestInfoDTO) {
 
+        return "";
+    }
 
+    @Override
+    public String GetDummyDateForAdvanced(User user, DummyRequestDTO.RequestInfoDTO requestInfoDTO) {
+        return "";
+    }
+
+    @Override
+    public String GetDummyDateForDanger(User user, DummyRequestDTO.RequestInfoDTO requestInfoDTO) {
         return "";
     }
 
