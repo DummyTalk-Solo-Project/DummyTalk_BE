6395d932e38a0c63c744ccac5ff5c9d288710463
jijysun <jijysun@gmail.com>
2025-09-16 23:45:35 +0900

    [FEAT] : 오늘 너무 바빠써 로그만이라도....

--- src/main/java/DummyTalk/DummyTalk_BE/domain/service/dummy/impl/DummyServiceImpl.java
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/service/dummy/impl/DummyServiceImpl.java
@@ -37,7 +37,7 @@ public class DummyServiceImpl implements DummyService {
     String content = "현 요청은 메타픽션 Spring 사이드 프로젝트에서 비회원 사용자가 잡상식을 구하는 요청이다.\n" +
             "해당 웹 사이트의 컨셉은 계속해서 새로고침 하다가 보면 일정 확률로 사용자 기반 데이터를 가지고 잡상식을 요청하면서 메타픽션을 다루게 될 것.\n" +
             "사전 설정을 일단 잘 알아두고, 수많은 주제에 대한 랜덤의 잡상식을 요청한다. 응답 잡상식은 다음 사항을 무조건 따라야 한다.\n" +
-            "1. 답변은 최소 90자 이상 120 글자 내로 답해야 한다. \n" +
+            "1. 답변은 최소 90자 이상 120 글자 내로 답해야 한다, 또한 문장를 완벽하게 끝마무리 지어야 한다.\n" +
             "2. 답변의 말투는 ~~요를 사용하여 친근하면서도 차갑지 않은 중립적의 말투를 사용할 것\n";
 
     @Override
@@ -54,6 +54,7 @@ public class DummyServiceImpl implements DummyService {
         User user = userRepository.findByEmail(reqUser.getEmail()).orElseThrow(RuntimeException::new);
 
         if (user.getInfo().getReqCount() >= 10){
+            log.info("{} -> 무료 이용 횟수 모두 소모!", user.getEmail());
             return "무료 이용 횟수를 모두 이용하셨네요. 다음을 기약해주세요 :)";
         }
 
@@ -73,6 +74,7 @@ public class DummyServiceImpl implements DummyService {
         random.setSeed(System.currentTimeMillis());
 
         if (random.nextInt(4) == 0){ // 20%의 확률로
+            log.info("사용자의 정보를 사용합니다.");
             isUserContent = true;
             content.concat("\n3. 다음은 사용자의 정보이다, 사용자 데이터 기반 잡상식을 만들 것, 위 사항은 정확히 따를 것" + reqUser + ", " + userContent);
         }
