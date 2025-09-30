06a2fc28c650337c451ea5dff85490aedf435faf
jijysun <jijysun@gmail.com>
2025-09-30 17:04:23 +0900

    [FEAT] : ai 프롬프트 상수화

--- src/main/java/DummyTalk/DummyTalk_BE/domain/service/dummy/impl/DummyServiceImplV2.java
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/service/dummy/impl/DummyServiceImplV2.java
@@ -8,6 +8,7 @@ import DummyTalk.DummyTalk_BE.domain.dto.quiz.QuizResponseDTO;
 import DummyTalk.DummyTalk_BE.domain.entity.Dummy;
 import DummyTalk.DummyTalk_BE.domain.entity.Quiz;
 import DummyTalk.DummyTalk_BE.domain.entity.User;
+import DummyTalk.DummyTalk_BE.domain.entity.constant.AIPrompt;
 import DummyTalk.DummyTalk_BE.domain.entity.constant.QuizStatus;
 import DummyTalk.DummyTalk_BE.domain.entity.mapping.UserQuiz;
 import DummyTalk.DummyTalk_BE.domain.repository.DummyRepository;
@@ -53,11 +54,7 @@ public class DummyServiceImplV2 implements DummyService {
     private final QuizRepository quizRepository;
     private final UserQuizRepository userQuizRepository;
     private final RedisTemplate<String, Object> redisTemplate;
-    private String content = "현 요청은 메타픽션 Spring 프로젝트에서 비회원 사용자가 잡상식을 구하는 요청이다. \n 사이트 컨셉은 계속해서 새로고침 하다가 보면 일정 확률로 사용자 기반 데이터를 가지고 잡상식을 요청하면서 메타픽션을 다루게 될 것.\n" +
-            "사전 설정을 일단 잘 알아두고, 수많은 주제에 대한 랜덤의 잡상식을 요청한다. 응답 잡상식은 다음 사항을 무조건 따라야 한다.\n" +
-            "1. 답변은 최소 90자 이상 120 글자 내로 답해야 한다, 또한 문장를 완벽하게 끝마무리 지어야 한다.\n" +
-            "2. 답변의 말투는 ~~요를 사용하여 친근하면서도 차갑지 않은 중립적의 말투를 사용할 것\n" +
-            "3. 또한 사용자 데이터 사용 시 사용자를 아는 척 하지 말고, ";
+
     @Value("${spring.ai.openai.api-key}")
     private String openAiKey;
 
@@ -91,10 +88,10 @@ public class DummyServiceImplV2 implements DummyService {
 
             log.info("사용자의 정보를 사용합니다.");
             isUserContent = true;
-            newRequest = content.concat("\n3. 다음은 사용자의 정보이다, 사용자 데이터 기반 잡상식을 만들 것, 위 사항은 정확히 따를 것" + reqUser + ", " + userContent + ", userInfo: " + userInfo);
+            newRequest = AIPrompt.GET_DUMMY_PROMPT.concat("\n3. 다음은 사용자의 정보이다, 사용자 데이터 기반 잡상식을 만들 것, 위 사항은 정확히 따를 것" + reqUser + ", " + userContent + ", userInfo: " + userInfo);
         }
 
-        ChatResponse resp = chatModel.call(new Prompt(newRequest == null ? content : newRequest,
+        ChatResponse resp = chatModel.call(new Prompt(newRequest == null ? AIPrompt.GET_DUMMY_PROMPT : newRequest,
                 OpenAiChatOptions.builder()
                         .model(OpenAiApi.ChatModel.GPT_4_O_MINI)
                         .maxTokens(100)
@@ -103,7 +100,7 @@ public class DummyServiceImplV2 implements DummyService {
         Dummy newDummy = Dummy.builder()
                 .user(user)
                 .isUserContent(isUserContent)
-                .request(content)
+                .request(AIPrompt.GET_DUMMY_PROMPT)
                 .response(resp.getResult().getOutput().getText())
                 .build();
         dummyRepository.save(newDummy);
@@ -134,13 +131,7 @@ public class DummyServiceImplV2 implements DummyService {
         DummyRequestDTO.GetDummyQuizDTO dto = DummyRequestDTO.GetDummyQuizDTO.builder()
                 .model("gpt-4o-mini")
                 .messages(List.of(new DummyRequestDTO.Message("user",
-                        "헷갈릴 수 있는 약간 어려운 잡학상식 문제를 낼 것, 다음 요청사항을 정확히 따를 것  ->  " +
-                                "1. 답변은 JSON 형식에 맞춰 답변할 것 {\"title\": \"니가 내는 문제\", \"answerList\": [\"정답1\", \"정답2\", \"정답3\", \"정답4\"], \"answer\": 정답번호, \"description\": \"정답에 대한 설명\"}, " +
-                                "2. 답변 말투는 ~요?를 사용하여 친근하면서도 차갑지 않은 중립적의 말투를 사용할 것, " +
-                                "3. 답변 속의 문제와 4개의 보기는 title과 answerList, answer에 정확히 담을 것, 또한 ```json 등의 코드 블록 문자도 제거할 것, " +
-                                "4. 최소 70 글자 이상의 문제를 낼 것. 이 규칙을 제일 중요하게 여길 것. " +
-                                "5. 정답에 대한 설명을 간단하게 적고 description 에 담을 것, " +
-                                "이후 JSON은 Java class {String title, List<String> answerList, Integer answer} 로 파싱할 예정이니 형식을 엄수할 것")))
+                        AIPrompt.GET_QUIZ_PROMPT)))
                 .max_tokens(200)
                 .build();
 
