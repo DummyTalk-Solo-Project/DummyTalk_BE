d347de160fa1d7afd04a4d1c0f47d060593c2942
jijysun <jijysun@naver.com>
2025-10-07 02:24:27 +0900

    [FEAT] : 잡상식 요청 동시성 고려

--- src/main/java/DummyTalk/DummyTalk_BE/domain/repository/UserRepository.java
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package DummyTalk.DummyTalk_BE.domain.repository;
 
 import DummyTalk.DummyTalk_BE.domain.entity.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,6 +20,10 @@ public interface UserRepository extends JpaRepository<User,Integer> {
 
     @Query("SELECT u FROM User u JOIN FETCH Info i ON i.user.id = u.id WHERE u.email = :email")
     Optional<User>findByEmailFetchInfo(@Param("email") String email);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT u FROM User u JOIN FETCH Info i ON i.user.id = u.id WHERE u.email = :email")
+    Optional<User>findByEmailFetchInfoWithLock(@Param("email") String email);
 
     Optional<User> findByEmailAndPassword(String email, String password);
 }
