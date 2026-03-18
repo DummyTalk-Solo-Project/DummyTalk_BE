ad66264f2b45ace73b1964c57345a0bf2cda4308
jijysun <jijysun@naver.com>
2026-03-18 19:17:50 +0900

    feat - Rarity 추가

---
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/entity/Rarity.java
@@ -0,0 +1,23 @@
+package DummyTalk.DummyTalk_BE.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class Rarity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String colorCode;
+
+    @Column(nullable = false)
+    private Double probability;
+}
