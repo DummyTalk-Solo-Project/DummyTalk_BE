2de24e042da423420dd2380722b950e4777165b4
jijysun <jijysun@gmail.com>
2026-03-24 16:44:23 +0900

    common - 성공 코드 수정

--- src/main/java/DummyTalk/DummyTalk_BE/global/apiResponse/status/SuccessCode.java
+++ src/main/java/DummyTalk/DummyTalk_BE/global/apiResponse/status/SuccessCode.java
@@ -10,15 +10,15 @@ public enum SuccessCode  {
     OK (HttpStatus.OK, "COMMON2000", "성공입니다"),
 
 
-    // User
-    LOGIN_SUCCESS(HttpStatus.OK, "USER2001", "로그인에 성공했습니다."),
-    LOGOUT_SUCCESS(HttpStatus.OK, "USER2002", "로그아웃에 성공했습니다."),
-    SIGN_IN_SUCCESS(HttpStatus.CREATED, "USER2003", "회원가입에 성공했습니다."),
-    EMAIL_SEND_SUCCESS(HttpStatus.OK, "USER2004", "인증 이메일 발송에 성공했습니다."),
-    VALIDATE_SUCCESS (HttpStatus.CONTINUE, "USER2005", "이메일 검증에 성공했습니다"),
-    WITHDRAWN_SUCCESS(HttpStatus.OK, "USER2006", "회원 탈퇴에 성공했씁니다."),
-    GET_INFO_SUCCESS(HttpStatus.OK, "USER2007", "사용자 정보 조회에 성공했습니다."),
-    SUBSCRIBE_SUCCESS(HttpStatus.OK, "USER2007", "구독 요청에 성공했습니다."),
+    // MEMBER
+    LOGIN_SUCCESS(HttpStatus.OK, "MEMBER2001", "로그인에 성공했습니다."),
+    LOGOUT_SUCCESS(HttpStatus.OK, "MEMBER2002", "로그아웃에 성공했습니다."),
+    SIGN_IN_SUCCESS(HttpStatus.CREATED, "MEMBER2003", "회원가입에 성공했습니다."),
+    EMAIL_SEND_SUCCESS(HttpStatus.OK, "MEMBER2004", "인증 이메일 발송에 성공했습니다."),
+    VALIDATE_SUCCESS (HttpStatus.CONTINUE, "MEMBER2005", "이메일 검증에 성공했습니다"),
+    WITHDRAWN_SUCCESS(HttpStatus.OK, "MEMBER2006", "회원 탈퇴에 성공했씁니다."),
+    GET_INFO_SUCCESS(HttpStatus.OK, "MEMBER2007", "사용자 정보 조회에 성공했습니다."),
+    SUBSCRIBE_SUCCESS(HttpStatus.OK, "MEMBER2007", "구독 요청에 성공했습니다."),
 
     // Dummy
     GET_DUMMY_SUCCESS(HttpStatus.OK, "DUMMY2000", "더미 요청에 성공하셨습니다."),
