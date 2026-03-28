e5f1a42df72dd73b5899dec828a8412f98c77082
jijysun <jijysun@naver.com>
2026-03-28 23:05:15 +0900

    scheduler - 하루 데이터 정산 스케쥴러 추가

--- src/main/java/DummyTalk/DummyTalk_BE/domain/repository/jpa/MemberDummyRepository.java
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/repository/jpa/MemberDummyRepository.java
@@ -5,6 +5,8 @@ import DummyTalk.DummyTalk_BE.domain.entity.mapping.MemberDummy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,4 +15,9 @@ public interface MemberDummyRepository extends JpaRepository<MemberDummy, Long> 
     @Query("select d from Dummy d join fetch d.rarity where d.id in :idList")
     List<Dummy> findAllByDummyIdList (Collection<Long> idList);
 
+    long countMemberDummyByCreatedAtBetween(LocalDateTime createdAtAfter, LocalDateTime createdAtBefore);
+
+
+    @Query("select md.id from MemberDummy md where md.createdAt between :createdAtAfter and :createdAtBefore")
+    List<Long> findAllByCreatedAtToday (LocalDateTime createdAtAfter, LocalDateTime createdAtBefore);
 }
--- src/main/java/DummyTalk/DummyTalk_BE/domain/repository/jpa/MemberRepository.java
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/repository/jpa/MemberRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,4 +35,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmailAndPassword(String email, String password);
 
     boolean existsByEmail(String mail);
+
+    long countMemberByCreatedAtBetween(LocalDateTime createdAtAfter, LocalDateTime createdAtBefore);
 }
