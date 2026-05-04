ac39ced20385342a2a6b171bd5237e85ce881fc4
jijysun <jijysun@naver.com>
2026-05-04 23:40:17 +0900

    badge - 엔티티 설명 주석 추가

--- src/main/java/DummyTalk/DummyTalk_BE/domain/entity/Badge.java
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/entity/Badge.java
@@ -9,13 +9,13 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-public class Badge extends CommonEntity {
+public class Badge extends CommonEntity { // CommonEntity -> 뱃지 취득일 확인 가능
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
-    private String name;
+    private String name; // 실질적인 뱃지 이름. ex '열정적인 더미톡유저'
 
     @Column(unique = true, nullable = false)
-    private String content;
+    private String content; // 뱃지 취득 이유. ex "더미를 100번 조회하셨어요! 그래도 스몰토크 정도의 잡상식은 늘어났을 거에요!"
 }
