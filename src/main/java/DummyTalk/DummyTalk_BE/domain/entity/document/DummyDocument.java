bebf7c581d162205b4fca031d2e7b2de4d2f82d2
jijysun <jijysun@naver.com>
2026-05-28 23:51:32 +0900

    ES - ES는 JPA Auditing 실행 X -> 수정

--- src/main/java/DummyTalk/DummyTalk_BE/domain/entity/document/DummyDocument.java
+++ src/main/java/DummyTalk/DummyTalk_BE/domain/entity/document/DummyDocument.java
@@ -15,13 +15,16 @@ import org.springframework.data.elasticsearch.annotations.Setting;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Document(indexName = "dummy")
 @Setting(settingPath = "elasticsearch/settings.json")
-public class DummyDocument extends CommonEntity {
+public class DummyDocument {
     @Id
     private Long id;
 
     // analyzer=suggest_analyzer: 색인 시 Nori 형태소 + edge_ngram(1~10)으로 [강, 강아, 강아지] 형태로 저장 → 자동완성·부분일치 지원
     // searchAnalyzer=nori: 검색 시 [강아지] 토큰이 edge_ngram 토큰과 매칭되도록 한글 형태소 기반 검색
     // standard를 쓰면 공백 기준 분리만 하므로 nori가 생성한 한글 형태소 토큰과 매칭 신뢰도 하락
+
+    private Long rarityId;
+
     @Field(type = FieldType.Text, analyzer = "suggest_analyzer", searchAnalyzer = "nori")
     private String title;
 
