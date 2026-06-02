b7f36af5c39bc53d565fb67ac2b32fa954079bc8
jijysun <jijysun@gmail.com>
2026-06-02 15:46:23 +0900

    Badge - getDummy() 트랜잭션 이후 이벤트 발생

--- src/main/java/DummyTalk/DummyTalk_BE/global/config/RedisConfig.java
+++ src/main/java/DummyTalk/DummyTalk_BE/global/config/RedisConfig.java
@@ -11,8 +11,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class RedisConfig {
