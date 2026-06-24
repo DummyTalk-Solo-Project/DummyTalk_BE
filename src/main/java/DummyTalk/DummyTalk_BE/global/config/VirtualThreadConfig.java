862dab97acb89d24f07a1b2ee49d054435af9c38
jijysun <jijysun@naver.com>
2026-06-24 23:48:49 +0900

    VT - VirtualThreadConfig 설정

--- src/main/java/DummyTalk/DummyTalk_BE/global/config/VirtualThreadConfig.java
+++ src/main/java/DummyTalk/DummyTalk_BE/global/config/VirtualThreadConfig.java
@@ -1,17 +1,17 @@
 package DummyTalk.DummyTalk_BE.global.config;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.Executors;
 
 @Configuration
 public class VirtualThreadConfig {
-    /*
-    *  [TODO 구현 순서]
-   Step 1. Tomcat Virtual Thread Executor 설정
-   Step 2. Async Executor (BadgeExecutor, mailExecutor) VirtualThreadTaskExecutor 교체
-   Step 3. TaskScheduler VirtualThreadTaskScheduler로 교체
-   Step 4. -Djdk.tracePinnedThreads=full 으로 Pinning 진단
-   Step 5. K6 Before/After 측정 및 포트폴리오 문서화
-    * */
 
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> virtualThreadCustomizer() {
+        return protocolHandler ->
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    }
 }
